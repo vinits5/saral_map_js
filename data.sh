be77f737ecb53cb8066b90e840991bf1921b244f
vinits5 <vinitsarode5@gmail.com>
2021-05-15 18:22:20 +0530

    json files will get auto deleted

--- data.sh
+++ data.sh
@@ -4,4 +4,6 @@ python3 utils/json2js_state.py
 python3 utils/split_districts_geojson.py
 python3 utils/json2js_districts.py
 
+rm -r datasets/india/india_district/districts/json/
+
 echo "Dataset generated!"
